88e6a6ab07f4517f7cef54fb00b4fe70f5d8bba3
Ayah-AQ <ayoya_1998@hotmail.com>
2023-07-02 23:02:41 +0000

    edit

--- routes/user.js
+++ routes/user.js
@@ -1,34 +1,44 @@
-'use strict'
-const bcrypt = require('bcrypt')
-const basicAuth = require('../src/Auth/basic')
-const express = require('express')
-const usersRouter = express.Router()
-const { usersTable } = require('../src/Models/index')
+'use strict';
+const bcrypt = require('bcrypt');
+const basicAuth = require('../src/Auth/basic');
+const express = require('express');
+const usersRouter = express.Router();
+const { usersTable } = require('../src/Models');
 
-usersRouter.use(express.json())
+usersRouter.use(express.json());
 
-usersRouter.post('/signup', basicAuth, signUpHandler);
-usersRouter.post('/signin', basicAuth, signInHandler)
+usersRouter.post('/signup', signUpHandler);
+usersRouter.post('/signin', signInHandler);
 
-async function signInHandler(req,res) {
-  const foundUser = await usersTable.findOne({where: {username: req.body.username}});
-  if(foundUser) {
-      const validUser = await bcrypt.compare(req.body.password, foundUser.password)
-      if(validUser) {
-          res.status(201).send(foundUser)
+async function signInHandler(req, res) {
+  try {
+    const foundUser = await usersTable.findOne({ where: { username: req.body.username } });
+    if (foundUser) {
+      const validUser = await bcrypt.compare(req.body.password, foundUser.password);
+      if (validUser) {
+        res.status(201).send(foundUser);
       } else {
-          throw new Error('Invalid')
+        res.status(401).json({ error: 'Invalid password' });
       }
-  } else {
-      throw new Error('Invalid Login')
+    } else {
+      res.status(401).json({ error: 'Invalid username' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
-async function signUpHandler (req,res) {
-  const newUser = await usersTable.create({
-    username: req.body.username,
-    password: req.body.password
-  })
-  res.status(201).send(newUser)
+
+async function signUpHandler(req, res) {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = await usersTable.create({
+      username: req.body.username,
+      password: hashedPassword
+    });
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
 
-module.exports = usersRouter
+module.exports = usersRouter;
--- src/Auth/basic.js
+++ src/Auth/basic.js
@@ -1,22 +1,26 @@
-'use strict'
+'use strict';
 const base64 = require('base-64');
 const bcrypt = require('bcrypt');
 
-function basicAuth (req, res, next) {
-    if(req.body.username) {
-        let password = bcrypt.hashSync(req.body.password, 5);
-        req.body.password = password
-        next()
+function basicAuth(req, res, next) {
+  if (req.body.username) {
+    next();
+  } else {
+    const userData = req.headers.authorization;
+    if (userData) {
+      let encodedData = userData.split(' ')[1];
+      let decodedData = base64.decode(encodedData).split(':');
+      let username = decodedData[0];
+      let password = decodedData[1];
+      req.user = {
+        username: username,
+        password: password
+      };
+      next();
     } else {
-        const userData = req.headers.authorization
-        if(userData) {
-            let encodedData = userData.split(" ")[1];
-            let decodedData = base64.decode(encodedData).split(":")
-            req.body.username = decodedData[0];
-            req.body.password = decodedData[1];
-            next();
-        }
+      next(new Error('Invalid login'));
     }
+  }
 }
 
-module.exports = basicAuth
+module.exports = basicAuth;
